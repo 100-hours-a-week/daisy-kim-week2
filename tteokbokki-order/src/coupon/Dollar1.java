1a8724990048c69b47bd2ee87ce0c5287ffabccd
okiidokim <127471879+okiidokim@users.noreply.github.com>
2025-02-12 17:42:42 +0900

    [FIX] 대문자 처리 재커밋
    왜 안 올라갔지...

--- tteokbokki-order/src/coupon/Dollar1.java
+++ tteokbokki-order/src/coupon/Dollar1.java
@@ -1,9 +1,9 @@
 package coupon;
 
 public class Dollar1 extends Coupon {
-    private static final int time = 60;
-    private static final int salePrice = 1000;
+    private static final int TIME = 60;
+    private static final int SALE_PRICE = 1000;
     public Dollar1() {
-        super(time, salePrice);
+        super(TIME, SALE_PRICE);
     }
 }
